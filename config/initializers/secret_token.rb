6c53354d537aa3219a503196812fc5abacc4a173
Tomohide Funayama <funayama.tomohide@members.co.jp>
2012-10-31 00:57:21 +0900

    add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Book::Application.config.secret_token = '65ef261459203c3ea716445a7f38fbc6e5735be02f5b3fc7b54e4d4df739d7c991bcb4dde006d9f72500833ed993b94e734e125b24ca8d85e7fe7c50eaa11d8e'
